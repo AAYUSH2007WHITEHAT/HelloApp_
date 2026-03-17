7324d693fd1e80c36be94810c05204e04413162e
AAYUSH2007WHITEHAT <iamaayushbagri@gmail.com>
2026-03-17 14:01:26 +0530

    coded uc3 in hello app

--- src/main/java/com/example/HelloApp.java
+++ src/main/java/com/example/HelloApp.java
@@ -1,10 +1,11 @@
 package HelloApp.src.main.java.com.example;
 
-public class HelloApp
-
-{
+public class UC3 {
     public static void main(String[] args) {
-        String name = "krs";
+        String name = "World";
+        if (args.length > 0) {
+            name = args[0];
+        }
         System.out.println("Hello " + name + "!");
     }
 }
